20a7b34471b4b808cf67ca5bcce2fea7bf3aecf1
KSentak <k.sentak@outlook.com>
2020-02-24 11:41:56 -0500

    added FOOTER component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../css/main.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer class="footer text-faded text-center py-5">
+        <div class="container">
+          <p class="m-0 small">Copyright &copy; KJS 2020</p>
+        </div>
+      </footer>
+    );
+  };
+};
+
+export default Footer;
