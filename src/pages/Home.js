4e0402ff2e516701cc7d1af4d3ea5ec6d11393a9
KSentak <k.sentak@outlook.com>
2020-02-24 11:39:39 -0500

    changed class to className in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,36 +12,36 @@ class Home extends Component {
 				<Header />
 				<Navbar />
 
-				<section class="page-section clearfix">
-					<div class="container">
-						<div class="intro">
-							<img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src={Barista} alt="Barista"/>
-							<div class="intro-text left-0 text-center bg-faded p-5 rounded">
-								<h2 class="section-heading mb-4">
-									<span class="section-heading-upper">Fresh Coffee</span>
-									<span class="section-heading-lower">Delicious to Drink</span>
+				<section className="page-section clearfix">
+					<div className="container">
+						<div className="intro">
+							<img className="intro-img img-fluid mb-3 mb-lg-0 rounded" src={Barista} alt="Barista"/>
+							<div className="intro-text left-0 text-center bg-faded p-5 rounded">
+								<h2 className="section-heading mb-4">
+									<span className="section-heading-upper">Fresh Coffee</span>
+									<span className="section-heading-lower">Delicious to Drink</span>
 								</h2>
-								<p class="mb-3">Each cup of our famous coffee is made with the freshest ingrediants. Come try our coffee
+								<p className="mb-3">Each cup of our famous coffee is made with the freshest ingrediants. Come try our coffee
 								  sometime and we promise that you'll fall in love with it!
           						</p>
-								<div class="intro-button mx-auto">
-									<a class="btn btn-primary btn-xl" target="_blank"  rel="noopener noreferrer" href="https://www.google.com/maps">Visit Us! (Maps Link)</a>
+								<div className="intro-button mx-auto">
+									<a className="btn btn-primary btn-xl" target="_blank"  rel="noopener noreferrer" href="https://www.google.com/maps">Visit Us! (Maps Link)</a>
 								</div>
 							</div>
 						</div>
 					</div>
 				</section>
 
-				<section class="page-section cta">
-					<div class="container">
-						<div class="row">
-							<div class="col-xl-9 mx-auto">
-								<div class="cta-inner text-center rounded">
-									<h2 class="section-heading mb-4">
-										<span class="section-heading-upper">Our Guarentee</span>
-										<span class="section-heading-lower">To You</span>
+				<section className="page-section cta">
+					<div className="container">
+						<div className="row">
+							<div className="col-xl-9 mx-auto">
+								<div className="cta-inner text-center rounded">
+									<h2 className="section-heading mb-4">
+										<span className="section-heading-upper">Our Guarentee</span>
+										<span className="section-heading-lower">To You</span>
 									</h2>
-									<p class="mb-0">When you come in to grab your morning cup of joe, we strive to provide the utmost
+									<p className="mb-0">When you come in to grab your morning cup of joe, we strive to provide the utmost
 									  satisfaction by providing the best ingrediants possible, a fantastic atmosphere, and excellent customer
 									  service.
             						</p>
